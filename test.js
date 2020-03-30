db5a163398c1d39ea4660c31676d4d2be4e967b9
Rubén <ruben.garcia@quobis.com>
2020-03-30 17:46:54 +0200

    chore: config jest and enzyme

--- test.js
+++ test.js
@@ -1,38 +1,38 @@
-const https = require('https');
-var querystring = require("querystring");
+// const https = require('https');
+// var querystring = require("querystring");
 
-const postData = querystring.stringify({
-    'token': ''
-});
+// const postData = querystring.stringify({
+//     'token': ''
+// });
 
-const options = {
-    host: 'slack.com',
-    port: 443,
-    path: '/api/dnd.endSnooze',
-    method: 'POST',
-    headers: {
-        'content-type': 'application/x-www-form-urlencoded',
-    },
+// const options = {
+//     host: 'slack.com',
+//     port: 443,
+//     path: '/api/dnd.endSnooze',
+//     method: 'POST',
+//     headers: {
+//         'content-type': 'application/x-www-form-urlencoded',
+//     },
 
-};
+// };
 
-// const google = {
-// }
-const res = https.request(options, (res) => {
-    console.log('statusCode:', res.statusCode);
-    console.log('headers:', res.headers);
-    res.setEncoding('utf8')
+// // const google = {
+// // }
+// const res = https.request(options, (res) => {
+//     console.log('statusCode:', res.statusCode);
+//     console.log('headers:', res.headers);
+//     res.setEncoding('utf8')
 
-    res.on('data', (d) => {
-        console.log('>>>>>>>>>>>> ', d);
-    });
-    res.on('end', () => console.log('finished'));
-
-
-})
+//     res.on('data', (d) => {
+//         console.log('>>>>>>>>>>>> ', d);
+//     });
+//     res.on('end', () => console.log('finished'));
 
 
-res.on('error', console.log)
+// })
 
-res.write(postData);
-res.end();
+
+// res.on('error', console.log)
+
+// res.write(postData);
+// res.end();
---
+++ tests/sum.spec.js
@@ -0,0 +1,11 @@
+describe('Test', function() {
+    beforeEach(() => {
+
+    });
+    afterEach(() => {
+
+    });
+    it('testing sum', () => {
+        expect(1).toBe(1)
+    });
+});
